c669b64d1ad8be309536624f01ca2a16c6d9f161
joki <josedavidamayar@gmail.com>
2024-09-04 02:36:58 -0500

    Call the menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Menu from "./components/Menu";
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        ¡Tailwind CSS está funcionando correcta!
-      </h1>
+    <div className="bg-custom-white min-h-screen">
+      <Menu />
     </div>
   );
 }
